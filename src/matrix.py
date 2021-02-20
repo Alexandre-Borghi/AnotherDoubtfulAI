fd1ae54728bbd21793cd6fea093eab38793c284b
Alexandre-Borghi <borghi.alexandre.12@gmail.com>
2021-02-20 21:08:26 +0100

    Added zeros classmethod to matrix + test

--- src/matrix.py
+++ src/matrix.py
@@ -1,70 +1,73 @@
+class MatrixError(Exception):
+    """An exception for the Matrix class"""
+
+    pass
+
+
 class Matrix:
     """This class represents a matrix of arbitrary size.
     
     Class methods:
-        identity: Creates an identity matrix of given dimension.
-        zeros: Creates a matrix of given size filled with zeros.
-        c_add: Adds 2 matrices together and returns the result.
+        identity: Creates an identity matrix of given rank.
+        zeros: Creates a matrix of given rank filled with zeros.
     
     Methods:
-        add: Adds a matrix to self and returns the result. Modifies self.
     """
 
-    def __init__(self, data=[[0]]):
+    def __init__(self, rows=[[0]]):
         """Constructs a matrix from a 2D array of numbers.
 
         Arguments:
-            data (2D array): Numbers to create the matrix from.
+            rows (2D array): Rows (arrays) to create the matrix from.
                 Default value is a 1x1 matrix with a 0.
         
         Returns:
-            A Matrix filled with the numbers from the data argument.
+            A Matrix filled with the numbers from the rows argument.
         
         Errors:
-            Raises an AssertionError if columns are not all the same size.
+            Raises an MatrixError if columns are not all the same size.
         """
 
-        for i in range(len(data) - 1):
-            assert len(data[i]) == len(data[i+1]),\
-                "all columns should have the same number of elements"
+        for i in range(len(rows) - 1):
+            if len(rows[i]) != len(rows[i + 1]):
+                raise MatrixError("Rows should all have the same lengths")
 
-        self.data = data
-        self.m = len(self.data)
-        self.n = len(self.data[0])
-    
+        self.rows = rows
+        self.m = len(self.rows)
+        self.n = len(self.rows[0])
+
     @classmethod
-    def identity(cls, dim=1):
-        """Returns the identity matrix of dimension dim.
+    def identity(cls, rank=1):
+        """Returns the identity matrix of rank 'rank'.
 
         Arguments:
-            dim (number): The dimension of the (square) matrix to create.
+            rank (number): The rank of the (square) matrix to create.
                 Default value is 1.
         
         Errors:
-            Raises an AssertionError if dim is 0 or less.
+            Raises an MatrixError if rank is 0 or less.
         """
-        assert dim > 0, "matrix dimension must be at least 1"
+        if rank < 1:
+            raise MatrixError("Matrix rank must be at least 1")
 
-        d = [[0 for _ in range(dim)] for _ in range(dim)]
+        rows = [[0 for _ in range(rank)] for _ in range(rank)]
 
-        for i in range(dim):
-            d[i][i] = 1
-        
-        return Matrix(d)
-    
+        for i in range(rank):
+            rows[i][i] = 1
+
+        return Matrix(rows)
+
     @classmethod
-    def c_add(cls, m1, m2):
-        """Adds 2 matrices of same size and returns the result.
-        
-        Arguments:
-            m1 (Matrix): First matrix of the addition.
-            m2 (Matrix): Second matrix of the addition.
-        
-        Returns:
-            A Matrix, the result of the addition.
-        """
-        
-        pass
+    def zeros(cls, rank=(1, 1)):
+        """Returns a zero-filled matrix of given rank.
 
-    def add(self, other):
-        pass
+        Arguments:
+            rank (tuple): The rank (m, n) of the matrix to create.
+
+        Returns:
+            A matrix of given rank filled with zeros.
+        """
+
+        rows = [[0 for _ in range(rank[1])] for _ in range(rank[0])]
+
+        return Matrix(rows)
--- src/test_matrix.py
+++ src/test_matrix.py
@@ -1,41 +1,66 @@
-import unittest
 import matrix
+import unittest
+
 
 class TestMatrix(unittest.TestCase):
     def test_init_default_value(self):
         mat = matrix.Matrix()
-        self.assertEqual(mat.data, [[0]], "empty matrix should contain a 0")
+        self.assertEqual(mat.rows, [[0]], "empty matrix should contain a 0")
         self.assertEqual(mat.m, 1)
         self.assertEqual(mat.n, 1)
-    
-    def test_init_with_data(self):
+
+    def test_init_with_rows(self):
         _ = matrix.Matrix([[0, 1, 2], [5, 7, -3], [-10, 8, 1]])
         _ = matrix.Matrix([[0.3, 2.1, 1.2], [7.5, 7, -3.25], [-11.54, 7, 45]])
-    
+
     @unittest.expectedFailure
     def test_init_with_different_sized_columns(self):
         _ = matrix.Matrix([[0, 2], [1], [8, 5, 6]])
-    
+
     def test_identity_default(self):
         mat = matrix.Matrix.identity()
 
-        self.assertEqual(mat.data, [[1]])
+        self.assertEqual(mat.rows, [[1]])
         self.assertEqual(mat.m, 1)
         self.assertEqual(mat.n, 1)
-    
+
     def test_identity(self):
         mat = matrix.Matrix.identity(2)
 
-        self.assertEqual(mat.data, [[1, 0], [0, 1]])
+        self.assertEqual(mat.rows, [[1, 0], [0, 1]])
         self.assertEqual(mat.m, 2)
         self.assertEqual(mat.n, 2)
 
     @unittest.expectedFailure
     def test_identity_with_negative_dimension(self):
         _ = matrix.Matrix.identity(-2)
-    
+
     def test_zeros_default(self):
-        pass
+        mat = matrix.Matrix.zeros()
+
+        self.assertEqual(mat.rows, [[0]])
+        self.assertEqual(mat.m, 1)
+        self.assertEqual(mat.n, 1)
+
+    def test_zeros(self):
+        mat = matrix.Matrix.zeros((2, 1))
+
+        self.assertEqual(mat.rows, [[0], [0]])
+        self.assertEqual(mat.m, 2)
+        self.assertEqual(mat.n, 1)
+
+        mat = matrix.Matrix.zeros((1, 2))
+
+        self.assertEqual(mat.rows, [[0, 0]])
+        self.assertEqual(mat.m, 1)
+        self.assertEqual(mat.n, 2)
+
+        mat = matrix.Matrix.zeros((3, 2))
+
+        self.assertEqual(mat.rows, [[0, 0], [0, 0], [0, 0]])
+        self.assertEqual(mat.m, 3)
+        self.assertEqual(mat.n, 2)
+
 
 if __name__ == "__main__":
     unittest.main()
