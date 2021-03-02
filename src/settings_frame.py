9548512d8410fc81bae5e5ae63916cbec0fe3f1a
Alexandre-Borghi <borghi.alexandre.12@gmail.com>
2021-03-02 16:37:47 +0100

    Used pack instead of place

--- src/settings_frame.py
+++ src/settings_frame.py
@@ -10,13 +10,11 @@ class SettingsFrame(tk.Frame):
         self.lr_slider = tk.Scale(
             self, from_=0.001, to=0.1, resolution=0.001, orient=tk.HORIZONTAL)
         self.lr_slider.set(0.1)
-        self.lr_slider.place(relx=0.01, rely=0.01,
-                             relwidth=0.98, relheight=0.1)
+        self.lr_slider.pack(fill=tk.X, padx=3, pady=3)
 
         self.reset_btn = tk.Button(
             self, text="Reset", command=reset_command)
-        self.reset_btn.place(relx=0.01, rely=0.12,
-                             relwidth=0.98, relheight=0.1)
+        self.reset_btn.pack(fill=tk.X, padx=3)
 
     def get_lr(self):
         return self.lr_slider.get()
